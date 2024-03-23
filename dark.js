7fbf3abf195a6c118e973a21a3d43134a1cc0d8d
kobbyyh <kwabenaacheampong812@gmail.com>
2024-03-24 03:17:56 +0530

    flora

--- dark.js
+++ dark.js
@@ -1,19 +1,19 @@
- // Function to toggle dark mode
- function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-    // Store the dark mode preference in local storage
-    const isDarkMode = body.classList.contains('dark-mode');
-    localStorage.setItem('darkMode', isDarkMode);
-  }
+//  // Function to toggle dark mode
+//  function toggleDarkMode() {
+//     const body = document.body;
+//     body.classList.toggle('dark-mode');
+//     // Store the dark mode preference in local storage
+//     const isDarkMode = body.classList.contains('dark-mode');
+//     localStorage.setItem('darkMode', isDarkMode);
+//   }
   
-  // Event listener for dark mode toggle button
-  document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
+//   // Event listener for dark mode toggle button
+//   document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
   
-  // Check local storage for dark mode preference on page load
-  document.addEventListener('DOMContentLoaded', function() {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    if (isDarkMode) {
-      document.body.classList.add('dark-mode');
-    }
-  });
+//   // Check local storage for dark mode preference on page load
+//   document.addEventListener('DOMContentLoaded', function() {
+//     const isDarkMode = localStorage.getItem('darkMode') === 'true';
+//     if (isDarkMode) {
+//       document.body.classList.add('dark-mode');
+//     }
+//   });
